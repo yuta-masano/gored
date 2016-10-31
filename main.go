579d646e6fe90101e412d1f3bb014e35cf7b2e2b
yuta-masano <masano@sraoss.co.jp>
2016-10-31 15:16:59 +0900

    ファイルを一時的に作成して題名と説明を書けるようにした
    godmine を大いに参考にしている。すごい。
    ioutil.WriteFile 関数で作成したファイルを run 関数で編集させているが、そ
    この解読は未だやっていない。
    
    issueFromEditor 関数がポイント。
    乱数で適当なファイル名を作成して、OS 毎に格納先を調整してる。
    格納先の調整はみんなの Go 言語と違って素朴に runtime.COOS で条件分岐させ
    ている。こっちの方が読みやすい気がする。
    
    上記の一時ファイルを格納するディレクトリを作成するようにした。
    os.Stat 関数を使って指定パスがファイル / ディレクトリとして存在するかど
    うか、存在するならそれがファイルかどうかをエラーチェックするようにしてあ
    る。
    よく考えたらパーミッションエラーの場合を考慮してなかったな。。。

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
 
+	"github.com/davecgh/go-spew/spew"
+	redmine "github.com/mattn/go-redmine"
 	"github.com/spf13/cobra"
 )
 
 var (
-	version bool
+	version     bool
+	projectID   string
+	tracker     string
+	subject     string
+	description string
+	priority    string
 
 	// These values are embedded when building.
 	buildVersion string
 	buildDate    string
 	buildWith    string
+)
+
+var (
+	trackerTable  = []string{"情報更新", "バグ", "機能", "サポート"}
+	priorityTable = []string{"Low", "Normal", "High"}
 )
 
 var rootCmd = &cobra.Command{
@@ -25,6 +45,10 @@ var rootCmd = &cobra.Command{
 func init() {
 	rootCmd.Flags().BoolVarP(&version, "version", "v", false,
 		"show program's version number and exit")
+	rootCmd.Flags().StringVarP(&tracker, "tracker", "t", "バグ",
+		fmt.Sprint("choose ", strings.Join(trackerTable, ", ")))
+	rootCmd.Flags().StringVarP(&priority, "priority", "p", "Normal",
+		fmt.Sprint("choose ", strings.Join(priorityTable, ", ")))
 }
 
 func runGored(cmd *cobra.Command, args []string) error {
@@ -32,6 +56,20 @@ func runGored(cmd *cobra.Command, args []string) error {
 		fmt.Printf("version: %s\nbuild at: %s\nwith: %s\n",
 			buildVersion, buildDate, buildWith)
 		return nil
+	}
+	if len(args) < 1 {
+		return fmt.Errorf("specify project_id to add a new issue\n")
+	}
+	if !contain(trackerTable, tracker) {
+		return fmt.Errorf("%s is invalid tracker\n", tracker)
+	}
+	if !contain(priorityTable, priority) {
+		return fmt.Errorf("%s is invalid priority\n", priority)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	if err := createIssue(); err != nil {
+		return fmt.Errorf("%s\n", err)
 	}
 
 	return nil
@@ -41,4 +79,131 @@ func main() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(-1)
 	}
+}
+
+func contain(haystack []string, needle string) bool {
+	for _, v := range haystack {
+		if needle == v {
+			return true
+		}
+	}
+	return false
+}
+
+func createIssue() error {
+	issue, err := issueFromEditor("")
+	if err != nil {
+		return err
+	}
+	spew.Dump(issue)
+	// c := redmine.NewClient(conf.Endpoint, conf.Apikey)
+	// issue.ProjectId = projectID
+	// _, err = c.CreateIssue(*issue)
+	// if err != nil {
+	// 	return err
+	// }
+	return nil
+}
+
+func issueFromEditor(contents string) (*redmine.Issue, error) {
+	file := ""
+	newf := fmt.Sprintf("%d.txt", rand.Int())
+	if runtime.GOOS == "windows" {
+		file = filepath.Join(os.Getenv("APPDATA"), ".config", "gored", newf)
+	} else {
+		file = filepath.Join(os.Getenv("HOME"), ".config", "gored", newf)
+	}
+	defer os.Remove(file)
+
+	editor := getEditor()
+
+	if contents == "" {
+		contents = `### Subject Here ###
+### Description Here ###
+`
+	}
+
+	if err := mkdir(filepath.Dir(file)); err != nil {
+		return nil, err
+	}
+	ioutil.WriteFile(file, []byte(contents), 0600)
+	if err := run([]string{editor, file}); err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	text := string(b)
+
+	if text == contents {
+		return nil, errors.New("Canceled")
+	}
+	lines := strings.Split(text, "\n")
+	if len(lines) == 0 {
+		return nil, errors.New("Canceled")
+	}
+	var subject, description string
+	if len(lines) == 1 {
+		subject = lines[0]
+	} else {
+		subject, description = lines[0], strings.Join(lines[1:], "\n")
+	}
+	var issue redmine.Issue
+	issue.Subject = subject
+	issue.Description = description
+	return &issue, nil
+}
+
+func getEditor() string {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		if runtime.GOOS == "windows" {
+			editor = "notepad"
+		} else {
+			editor = "vi"
+		}
+	}
+	return editor
+}
+
+func run(args []string) error {
+	cmd, err := exec.LookPath(args[0])
+	if err != nil {
+		return err
+	}
+	var stdin *os.File
+	if runtime.GOOS == "windows" {
+		stdin, _ = os.Open("CONIN$")
+	} else {
+		stdin = os.Stdin
+	}
+	p, err := os.StartProcess(cmd, args, &os.ProcAttr{Files: []*os.File{
+		stdin,
+		os.Stdout,
+		os.Stderr,
+	}})
+	if err != nil {
+		return err
+	}
+	defer p.Release()
+	w, err := p.Wait()
+	if err != nil {
+		return err
+	}
+	if !w.Exited() || !w.Success() {
+		return errors.New("Failed to execute text editor")
+	}
+	return nil
+}
+
+func mkdir(dir string) error {
+	finfo, err := os.Stat(dir)
+	if err != nil { // err がある = no such file or directory のはず。
+		os.Mkdir(dir, 0700)
+	} else if !finfo.IsDir() {
+		return fmt.Errorf("%s mast be directory", dir)
+	}
+	return nil
 }
